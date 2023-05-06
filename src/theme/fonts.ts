6d3a163b258a222d5eb0a9012593ade741a05a62
Bora <bora@Bora-MacBook-Air.local>
2023-05-06 12:34:13 +0300

    initialize porject & install vector icons

---
+++ src/theme/fonts.ts
@@ -0,0 +1,18 @@
+const size = {
+  xs: 10,
+  sm: 12,
+  default: 14,
+  md: 16,
+  lg: 20,
+  xl: 24,
+  xxlg: 32,
+};
+
+const weight = {
+  full: '900',
+  bold: 'bold',
+  normal: 'normal',
+  thin: '400',
+};
+
+export default {size, weight};
